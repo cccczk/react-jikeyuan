e186d5e13cf6ba52a9af68cdee8dcb4090156cf9
陈志坤 <923854202@qq.com>
2025-02-05 14:43:04 +0800

    登录 -表单校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,71 @@
+import { Card } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import "./index.scss";
+import logo from "@/assets/logo.png"
 const Login = () => {
-  return <div>woshi Login</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  const logoUrl = "../"
+  return (
+    <div className="login">
+      <div className="loginContainer">
+        <Card
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <Form
+            name="basic"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            validateTrigger="onBlur"
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "请输入手机号!",
+                },
+                {
+                  pattern: /^1[3-9]\d{9}$/,
+                  message: "手机号码格式不对",
+                },
+                
+              ]}
+            >
+              <Input size="large" placeholder="请输入手机号"></Input>
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "请输入密码!",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="请输入密码"></Input>
+            </Form.Item>
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit" size="large" block>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
